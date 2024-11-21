2b68f34f1e67b0ce6953f795b2516a282ad167c0
Lihuayang05 <23013744@myrp.edu.sg>
2024-11-21 14:49:30 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -41,6 +41,8 @@ function Form({ addItem }) {
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
       </select>
       <input
         type="text"
